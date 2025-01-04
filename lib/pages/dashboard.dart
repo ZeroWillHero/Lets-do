8b478794b01917bb0781e339e5245c4734ff0256
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-05 03:19:15 +0530

    Dashboard created

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:letsdo/components/tasks_details.dart';
+import 'package:letsdo/components/weather_card.dart';
 import 'package:letsdo/theme_data.dart';
 import '../components/progress_card.dart';
 
@@ -8,35 +10,40 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
 
     return Padding(
-      padding: EdgeInsets.fromLTRB(20,  screenHeight * 0.1, 20, 20),
-      
-      child: Container(
-          height: MediaQuery.of(context).size.height,
-          
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.dashboard, color: textColor, size: 30),
-                  SizedBox(width:5),
-                  Text('Dashboard',
-                      style: TextStyle(fontSize: 30, color: textColor)),
-
-
-                ],
-              ),
-
+      padding: EdgeInsets.fromLTRB(20, screenHeight * 0.1, 20, 20),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.dashboard, color: textColor, size: 30),
+                SizedBox(width: 5),
+                Text('Dashboard',
+                    style: TextStyle(fontSize: 30, color: textColor)),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            WeatherCard(),
+            SizedBox(
+              height: 20,
+            ),
             ProgressCard(),
 
-            ],
+            SizedBox(
+              height: 20,
+            ),
 
-          ),
+            TaskDetailsCard(),
+          ],
+        ),
       ),
     );
   }
